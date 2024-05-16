7b42278cdb93581558a135ed54d6c0e8d2405c26
Ankit singh <singhankit0460@gmail.com>
2024-05-16 15:08:47 +0530

    final update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,33 @@ import Layout from '../component/Layout'
 function About() {
   return (
     <Layout>
-      <h1>About</h1>
+      <section className="about">
+        <h2>Welcome To My Resturant </h2>
+        <p>
+          We are a team of passionate developers who are committed to building
+          high-quality, user-friendly software. We have a proven track record of
+          success in delivering innovative and reliable solutions to our clients.
+          We are a team of passionate developers who are committed to building
+          high-quality, user-friendly software. We have a proven track record of
+          success in delivering innovative and reliable solutions to our clients.
+        </p>
+        <p>
+          We believe that the best software is created through collaboration and
+          communication. We work closely with our clients to understand their needs
+          and ensure that we are delivering the best possible solution.
+          We are a team of passionate developers who are committed to building
+          high-quality, user-friendly software. We have a proven track record of
+          success in delivering innovative and reliable solutions to our clients.
+        </p>
+        <p>
+          We are always looking for new challenges and opportunities. If you have a
+          project that you think we would be a good fit for, please don't hesitate to
+          contact us.
+          We are a team of passionate developers who are committed to building
+          high-quality, user-friendly software. We have a proven track record of
+          success in delivering innovative and reliable solutions to our clients.
+        </p>
+      </section>
     </Layout>
   )
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,40 @@
+import React from "react";
+import { MenuList } from "../data/Data";
+import Layout from '../component/Layout'
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
-import Layout from "../component/Layout"
-function Menu() {
+const Menu = () => {
   return (
     <Layout>
-      <h1>Menu</h1>
+      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+        {MenuList.map((menu) => (
+          <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
+            <CardActionArea>
+              <CardMedia
+                sx={{ minHeight: "400px" }}
+                component={"img"}
+                src={menu.image}
+                alt={menu.name}
+              />
+              <CardContent>
+                <Typography variant="h5" gutterBottom component={"div"}>
+                  {menu.name}
+                </Typography>
+                <Typography variant="body2">{menu.description}</Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
